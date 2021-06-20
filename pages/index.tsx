8f759419100715c1a607cc7e071b88b3f6765562
tatsu513 <0285mashiko@gmail.com>
2021-06-20 15:13:46 +0900

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,18 @@
+import Head from 'next/head';
+
+const home = () => {
+  return (
+    <div>
+      <Head>
+        <title>BUCKET LIST</title>
+        <link
+          rel="icon"
+          href="https://emojipedia-us.s3.dualstack.us-west-1.amazonaws.com/thumbs/240/apple/285/unicorn_1f984.png"
+        />
+      </Head>
+      <main>bucket lost</main>
+    </div>
+  );
+};
+
+export default home;
