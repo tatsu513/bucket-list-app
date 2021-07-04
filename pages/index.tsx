fff2404f5d6d30ffaa56aa9f4968140f0ae49c4b
tatsu513 <0285mashiko@gmail.com>
2021-07-04 17:40:19 +0900

    change way to load

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import { useState, useCallback } from 'react';
 import { Filter, List } from '../src/components';
 import { FloatButton } from 'src/components/buttons/';
 import styles from 'src/assets/styles/modules/Home.module.scss';
-import AddModal from 'src/components/modals/addModal';
+import { AddModal } from 'src/components/modals';
 
 const home = () => {
   const [isOpen, setIsOpen] = useState(false);
