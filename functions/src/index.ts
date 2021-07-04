949a405a0fd6024bcffe65bc40b9cef9288b3d79
tatsu513 <0285mashiko@gmail.com>
2021-07-04 17:53:30 +0900

    functions/index.ts

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,4 +1,6 @@
-// import * as functions from "firebase-functions";
+import * as functions from "firebase-functions";
+
+console.log(functions);
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
