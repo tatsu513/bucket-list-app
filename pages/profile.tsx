62e3cdfbfe4aacdf88a62dd2c72fff4bae09d645
tatsu513 <0285mashiko@gmail.com>
2021-08-22 15:29:02 +0900

    プロフィール画像を登録できるように

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -2,12 +2,12 @@ import styles from 'src/assets/styles/modules/Profile.module.scss';
 import firebase from 'firebase/app';
 import { useRouter } from 'next/router';
 import { Header, PageTitle, PageWrapper } from 'src/components';
-import { Person, Face } from '@material-ui/icons';
+import { Person, Face, AddPhotoAlternate } from '@material-ui/icons';
 import { SecondaryButton } from 'src/components/buttons';
 import { useState } from 'react';
 import { MailEditModal, ProfileEditModal } from 'src/components/modals';
 import { useCallback, useEffect } from 'react';
-import { auth } from 'src/firebase';
+import { auth, storage } from 'src/firebase';
 import { getGenders, getUser } from 'src/api';
 import { Options, User } from 'src/types';
 import { getNameById } from 'src/util/common';
@@ -18,6 +18,7 @@ const Profile: React.VFC = () => {
 
   const [currentUser, setCurrentUser] = useState<firebase.User | null>(null);
   const [user, setUser] = useState<User | undefined>(undefined);
+  const [image, setImage] = useState('');
   const [genders, setGenders] = useState<Options[] | null>([]);
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [isOpenChangeMailModal, setIsOpenChangeMailModal] = useState(false);
@@ -35,9 +36,43 @@ const Profile: React.VFC = () => {
     setIsOpenChangeMailModal(false);
   }, []);
 
+  const changeProfileImage = useCallback(
+    (event: React.ChangeEvent<HTMLInputElement>) => {
+      if (!event.target.files) return;
+      const file = event.target.files[0];
+      const fileName = file.name;
+      const user = auth.currentUser;
+
+      if (!user) return;
+
+      const storageRef = storage.ref().child(`profile/${fileName}`);
+      const uploadTask = storageRef.put(file);
+
+      uploadTask.then(() => {
+        uploadTask.snapshot.ref.getDownloadURL().then((downloadUrl) => {
+          user.updateProfile({ photoURL: downloadUrl }).then(() => {
+            setImage(downloadUrl);
+          });
+        });
+      });
+    },
+    [],
+  );
+
+  const imagePath = {
+    backgroundImage: `url('${image}')`,
+    backgroundSize: '100%',
+  };
+
   useEffect(() => {
     auth.onAuthStateChanged((user) => {
-      user ? setCurrentUser(user) : router.push('/account/signin');
+      console.log(user);
+      if (!user) {
+        router.push('/account/signin');
+      } else {
+        setCurrentUser(user);
+        setImage(user.photoURL ? user.photoURL : '');
+      }
     });
     getGenders().then((genders) => setGenders(genders));
   }, []);
@@ -55,9 +90,20 @@ const Profile: React.VFC = () => {
       {user && (
         <div className={styles.profileBox}>
           <div className={styles.profileImageBox}>
-            <span className={styles.profileImage}>
-              <Face fontSize={'inherit'} />
+            <span className={styles.profileImage} style={imagePath}>
+              {image === '' && <Face fontSize={'inherit'} />}
             </span>
+            <div className={styles.addImage}>
+              <label className={styles.uploadLabel} htmlFor="file-upload">
+                <span className={styles.labelText}>画像を変更</span>
+                <input
+                  className={styles.uploadInput}
+                  id="file-upload"
+                  type="file"
+                  onChange={(event) => changeProfileImage(event)}
+                ></input>
+              </label>
+            </div>
           </div>
           <div className={styles.profileContent}>
             <dl>
