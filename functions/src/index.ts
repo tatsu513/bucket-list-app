40e4564a5a816081c73d2d5adfac20c6c2d0e935
tatsu513 <0285mashiko@gmail.com>
2021-08-23 00:46:35 +0900

    functionsでメールが送られるか実験

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,11 +1,49 @@
-import * as functions from "firebase-functions";
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+const nodemailer = require('nodemailer');
+const smtpTransport = require('nodemailer-smtp-transport');
+const cors = require('cors')({origin: true});
+const gmailEmail = functions.config().gmail.mail;
+const gmailPassword = functions.config().gmail.password;
 
-console.log(functions);
+// Start writing Firebase Functions
+// https://firebase.google.com/docs/functions/typescript
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+admin.initializeApp();
+
+// Using Gmail
+const transporter = nodemailer.createTransport(smtpTransport({
+  service: 'gmail',
+  auth: {
+    user: gmailEmail,
+    pass: gmailPassword,
+  },
+}));
+
+// Sending the request
+exports.sendMail = functions.https.onRequest((req: any, res: any) => {
+  cors(req, res, () => {
+    // const dest = req.query.dest;
+
+    const mailOptions = {
+      from: 'John Doe <example@example.com>',
+      to: '0285mashiko@gmail.com',
+      subject: 'Test Mail', // email subject
+      text: 'Plain Text Test Email',
+      html: '<h2>テストのメールだよ</h2>', // HTML Mail
+    };
+
+    return transporter.sendMail(mailOptions, (error: any, info: any) => {
+      if (error) {
+        return res.send(error.toString());
+      }
+
+      return res.send('Mail Sended');
+    });
+  });
+});
+
+export const helloWorld = functions.https.onRequest((request: any, response: any) => {
+  functions.logger.info('Hello logs!', { structuredData: true });
+  response.send('Hello from Firebase!');
+});
